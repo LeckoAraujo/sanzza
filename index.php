eeb7027b62fb8a8eb472330b3744b45806164e87
Leandro Edson Silva de Araujo <leandro.araujo@stc.ma.gov.br>
2022-11-06 15:36:35 -0300

    Feito até Inspiração

--- index.php
+++ index.php
@@ -136,6 +136,24 @@
     </div>
 </section>
 <!-- Bem Vindo -->
+
+<!-- Vista -->
+<section class="vista">
+    <div>
+        <img src="assets/images/homepage/nicola-rossio-hotel-bem-vindo-3-hover.jpg" alt="A vista do Hotel para o Rossio">
+        <img class="filter" src="assets/images/homepage/nicola-rossio-hotel-bem-vindo-3.jpg" alt="A vista do Hotel para o Rossio">
+    </div>
+    <p class="text-citation">AS VISTAS DO HOTEL PARA O ROSSIO</p>
+</section>
+<!-- Vista -->
+
+<!-- Inspiração -->
+<section class="inspiration">
+    <h2 class="mega-title">O NICOLA COMO <br> INSPIRAÇÃO</h2>
+    <p class="text-p">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+    <img src="assets/images/homepage/nicola-rossio-hotel-excerto-2.jpg" alt="Outro texto que não consegui ler">
+</section>
+<!-- Inspiração -->
     
 </body>
 </html>
